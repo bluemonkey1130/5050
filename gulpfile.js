28357eb949ab3ff503d0378811c178ec6c3ce176
George <bluemonkey1130@gmail.com>
2018-04-26 00:09:03 +0100

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    prefix = require('gulp-autoprefixer');
 
-gulp.task('sass', function(){
+
+var onError = function (err) {
+    notify.onError({
+        title: "Gulp",
+        subtitle: "Failure!",
+        message: "Error: <%= error.message %>",
+        sound: "Basso"
+    })(err);
+    this.emit('end');
+};
+
+var sassOptions = {
+    outputStyle: 'expanded'
+};
+
+
+var prefixerOptions = {
+    browsers: ['last 2 versions']
+};
+
+
+gulp.task('sass', function () {
     return gulp.src('scss/main.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sass(sassOptions)) // Converts Sass to CSS with gulp-sass
+        .pipe(prefix(prefixerOptions))
+
         .pipe(gulp.dest('css'))
 });
