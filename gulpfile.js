3daeecdd8d870f8827e87d69bd6101d97c83bd2f
George <bluemonkey1130@gmail.com>
2018-04-26 14:56:13 +0100

    added package json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp')
-sass = require('gulp-sass'),
+    sass = require('gulp-sass'),
     plumber = require('gulp-plumber'),
     prefix = require('gulp-autoprefixer'),
     cssmin      = require('gulp-cssnano'),
